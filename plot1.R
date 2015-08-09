eb83f5a89ca7bdaa19f0ea994f4f35d315aaac1b
notsaralt <michael.dennis.pruitt@gmail.com>
2015-08-09 10:56:29 -0400

    7

--- plot1.R
+++ plot1.R
@@ -6,7 +6,7 @@ date2 <- as.Date('02/02/2007', "%d/%m/%Y")
 
 electric_data <- subset(electric_data, Date == date1 | Date == date2)
 
+png('plot1.png', width = 480, height = 480, units = 'px')
 hist(electric_data$Global_active_power, xlab = 'Global Active Power (kilowatts)', col = 'red', main = 'Global Active Power')
 
-dev.copy(png, './plot1.png')
 dev.off()
--- plot2.R
+++ plot2.R
@@ -11,8 +11,8 @@ date2 <- as.Date('02/02/2007', "%d/%m/%Y")
 electric_data <- subset(electric_data, Formatted_Date == date1 | Formatted_Date == date2)
 electric_data$Date_Time <- strptime(paste(electric_data$Date, electric_data$Time), format = "%d/%m/%Y %H:%M:%S") 
 
+png('plot2.png', width = 480, height = 480, units = 'px')
 plot(electric_data$Date_Time, electric_data$Global_active_power, ylab = 'Global Active Power (kilowatts)', xlab = '', type = 'n')
 lines(electric_data$Date_Time, electric_data$Global_active_power)
 
-dev.copy(png, './plot2.png')
 dev.off()
